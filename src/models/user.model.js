bc0b145ff0c2f80c82069439a6c0cc26821042e2
Divy Koushik Mishra <divykoushikmishra@gmail.com>
2024-03-17 18:17:55 +0530

    Remove username field from user registration

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,13 +4,13 @@ import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
+    // username: {
+    //   type: String,
+    //   required: true,
+    //   unique: true,
+    //   lowercase: true,
+    //   trim: true,
+    // },
     email: {
       type: String,
       required: true,
