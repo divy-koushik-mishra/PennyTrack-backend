7001144b666ec40b5997739220dc9b0b17fca99b
Divy Koushik Mishra <divykoushikmishra@gmail.com>
2024-03-19 18:25:56 +0530

    Add budget routes

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import transactionRoute from "./routes/transaction.routes.js";
 import incomeRoute from "./routes/income.routes.js";
 import expenseRoute from "./routes/expense.routes.js";
 import reminderRoute from "./routes/reminder.routes.js";
+import budgetRoutes from "./routes/budget.routes.js";
 
 // Routes declaration
 app.use("/api/v1/users", userRouter);
@@ -29,5 +30,6 @@ app.use("/api/v1/transactions", transactionRoute);
 app.use("/api/v1/income", incomeRoute);
 app.use("/api/v1/expense", expenseRoute);
 app.use("/api/v1/reminder", reminderRoute);
+app.use("/api/v1/budget", budgetRoutes);
 
 export { app };
